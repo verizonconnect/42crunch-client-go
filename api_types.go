0774649689902e1785e64f1827d89a97cb9d9894
Marais van Zyl <mieliespoor@users.noreply.github.com>
2023-06-19 20:11:50 +0100

    feat: api assessment report (#5)
    * feat: read assessment report

--- api_types.go
+++ api_types.go
@@ -101,3 +101,130 @@ type ApiStatus struct {
 	IsAssessmentProcessed bool
 	IsScanProcessed       bool
 }
+
+type ApiAssessmentResponse struct {
+	Tid      string    `json:"tid"`
+	Aid      string    `json:"aid"`
+	Cid      string    `json:"cid"`
+	Date     time.Time `json:"date"`
+	Data     string    `json:"data"`
+	Type     string    `json:"type"`
+	Encoding string    `json:"enc"`
+	Log      string    `json:"log"`
+}
+
+type AssessmentReport struct {
+	Index                   []string `json:"index"`
+	AssessmentVersion       string   `json:"assessmentVersion"`
+	AssessmentReportVersion string   `json:"assessmentReportVersion"`
+	Commit                  string   `json:"commit"`
+	OasVersion              string   `json:"oasVersion"`
+	APIVersion              string   `json:"apiVersion"`
+	FileID                  string   `json:"fileId"`
+	APIID                   string   `json:"apiId"`
+	OpenapiState            string   `json:"openapiState"`
+	Score                   float64  `json:"score"`
+	Valid                   bool     `json:"valid"`
+	Criticality             int      `json:"criticality"`
+	IssueCounter            int      `json:"issueCounter"`
+	SemanticErrors          struct {
+		Issues map[string]AssessmentIssue `json:"issues"`
+	} `json:"semanticErrors"`
+	ValidationErrors struct {
+		Issues map[string]AssessmentIssue `json:"issues"`
+	} `json:"validationErrors"`
+	Warnings struct {
+		Issues map[string]AssessmentIssue `json:"issues"`
+	} `json:"warnings"`
+	OperationsNoAuthentication []int `json:"operationsNoAuthentication"`
+	MinimalReport              bool  `json:"minimalReport"`
+	MaxEntriesPerIssue         int   `json:"maxEntriesPerIssue"`
+	MaxImpactedPerEntry        int   `json:"maxImpactedPerEntry"`
+	Security                   struct {
+		IssueCounter         int                        `json:"issueCounter"`
+		Score                int                        `json:"score"`
+		Criticality          int                        `json:"criticality"`
+		Issues               map[string]AssessmentIssue `json:"issues"`
+		SubgroupIssueCounter struct {
+			Authentication IssueCounter `json:"authentication"`
+			Authorization  IssueCounter `json:"authorization"`
+			Transport      IssueCounter `json:"transport"`
+		} `json:"subgroupIssueCounter"`
+	} `json:"security"`
+	Data struct {
+		IssueCounter         int                        `json:"issueCounter"`
+		Score                float64                    `json:"score"`
+		Criticality          int                        `json:"criticality"`
+		Issues               map[string]AssessmentIssue `json:"issues"`
+		SubgroupIssueCounter struct {
+			Parameters         IssueCounter `json:"parameters"`
+			ResponseHeader     IssueCounter `json:"responseHeader"`
+			ResponseDefinition IssueCounter `json:"responseDefinition"`
+			Schema             IssueCounter `json:"schema"`
+			Paths              IssueCounter `json:"paths"`
+		} `json:"subgroupIssueCounter"`
+	}
+	IssuesKey     []string                `json:"issuesKey"`
+	SkippedIssues []string                `json:"skippedIssues"`
+	Summary       AssessmentReportSummary `json:"summary"`
+}
+
+type AssessmentIssue struct {
+	Description string `json:"description"`
+	Issues      []struct {
+		Score               float64 `json:"score"`
+		Pointer             int     `json:"pointer"`
+		TooManyImpacted     bool    `json:"tooManyImpacted"`
+		Criticality         int     `json:"criticality"`
+		Response            bool    `json:"response"`
+		SpecificDescription string  `json:"specificDescription"`
+	} `json:"issues"`
+	TotalIssues  int     `json:"totalIssues"`
+	IssueCounter int     `json:"issueCounter"`
+	Score        float64 `json:"score"`
+	Criticality  int     `json:"criticality"`
+	TooManyError bool    `json:"tooManyError"`
+}
+
+type IssueCounter struct {
+	None     int `json:"none"`
+	Info     int `json:"info"`
+	Low      int `json:"low"`
+	Medium   int `json:"medium"`
+	High     int `json:"high"`
+	Critical int `json:"critical"`
+}
+
+type AssessmentReportSummary struct {
+	OasVersion                       string           `json:"oasVersion"`
+	APIVersion                       string           `json:"apiVersion"`
+	Basepath                         string           `json:"basepath"`
+	APIName                          string           `json:"apiName"`
+	Description                      string           `json:"description"`
+	Endpoints                        []string         `json:"endpoints"`
+	PathCounter                      int              `json:"pathCounter"`
+	OperationCounter                 int              `json:"operationCounter"`
+	ParameterCounter                 int              `json:"parameterCounter"`
+	RequestBodyCounter               int              `json:"requestBodyCounter"`
+	RequestContentType               map[string]int64 `json:"requestContentType"`
+	ResponseContentType              map[string]int64 `json:"responseContentType"`
+	ComponentsSchemasCounter         int              `json:"componentsSchemasCounter"`
+	ComponentsResponsesCounter       int              `json:"componentsResponsesCounter"`
+	ComponentsParametersCounter      int              `json:"componentsParametersCounter"`
+	ComponentsExamplesCounter        int              `json:"componentsExamplesCounter"`
+	ComponentsRequestBodiesCounter   int              `json:"componentsRequestBodiesCounter"`
+	ComponentsHeadersCounter         int              `json:"componentsHeadersCounter"`
+	ComponentsSecuritySchemesCounter int              `json:"componentsSecuritySchemesCounter"`
+	ComponentsLinksCounter           int              `json:"componentsLinksCounter"`
+	ComponentsCallbacksCounter       int              `json:"componentsCallbacksCounter"`
+}
+
+type ApiIssue struct {
+	Id           string
+	Description  string
+	Pointer      string
+	Score        int32
+	DisplayScore string
+	Criticality  int
+	Severity     string
+}
